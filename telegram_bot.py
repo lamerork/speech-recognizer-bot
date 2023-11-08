323271578c790a158121375c4bc8b03df4055f25
Anton Ozerov <lamerork@gmail.com>
2023-11-08 18:50:05 +0300

    Add log bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,33 @@
 from environs import Env
-from telegram import Update
+import logging
+from time import sleep
+import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 from dialogflow import detect_intent_texts
 
 
-def start(update: Update, context: CallbackContext) -> None:
+logger = logging.getLogger('Logger')
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, bot_name, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+        self.bot_name = bot_name
+
+    def emit(self, record):
+        log_entry = f'<b>{self.bot_name}:</b>\n{self.format(record)}'
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry, parse_mode=telegram.ParseMode.HTML)
+
+
+def start(update: telegram.Update, context: CallbackContext) -> None:
     update.message.reply_text('Здравствуйте!')
 
 
-def reply(update: Update, context: CallbackContext) -> None:
+def reply(update: telegram.Update, context: CallbackContext) -> None:
     session_id = update.effective_user['id']
     answer = detect_intent_texts(session_id, update.message.text, 'ru-RU')
     if answer == None:
@@ -22,13 +40,29 @@ def main():
     env.read_env()
 
     updater = Updater(env.str('TELEGRAM_TOKEN'))
+    logger_bot = telegram.Bot(token=env.str('TELEGRAM_LOG_TOKEN'))
+    chat_id = env.str('TELEGRAM_CHAT_ID')
+
     dispatcher = updater.dispatcher
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(logger_bot, 'ТГ Распознование речи', chat_id))
+
+    logger.info('Бот запущен')
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply))
 
-    updater.start_polling()
-    updater.idle()
+    try:
+        updater.start_polling()
+        updater.idle()
+
+    except ConnectionError:
+        logger.warning('Ошибка соединения')
+        sleep(60)
+
+    except Exception as err:
+        logger.exception(err)
 
 
 if __name__ == '__main__':
--- vk_bot.py
+++ vk_bot.py
@@ -1,27 +1,62 @@
 from environs import Env
 import random
+import logging
+from time import sleep
 
+import telegram
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 from dialogflow import detect_intent_texts
 
 
+logger = logging.getLogger('Logger')
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, bot_name, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+        self.bot_name = bot_name
+
+    def emit(self, record):
+        log_entry = f'<b>{self.bot_name}:</b>\n{self.format(record)}'
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry, parse_mode=telegram.ParseMode.HTML)
+
+
 def main():
     env = Env()
     env.read_env()
+
+    logger_bot = telegram.Bot(token=env.str('TELEGRAM_LOG_TOKEN'))
+    chat_id = env.str('TELEGRAM_CHAT_ID')
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(logger_bot, 'VK Распознование речи', chat_id))
+
+    logger.info('Бот запущен')
 
     vk_session = vk.VkApi(token=env.str('VK_TOKEN_GROUP'))
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            answer = detect_intent_texts(event.user_id, event.text, 'ru-RU')
-            if answer:
-                vk_api.messages.send(user_id=event.user_id,
-                                     message=answer,
-                                     random_id=random.randint(1, 1000))
+    try:
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                answer = detect_intent_texts(event.user_id, event.text, 'ru-RU')
+                if answer:
+                    vk_api.messages.send(user_id=event.user_id,
+                                         message=answer,
+                                         random_id=random.randint(1, 1000))
+
+    except ConnectionError:
+        logger.warning('Ошибка соединения')
+        sleep(60)
+
+    except Exception as err:
+        logger.exception(err)
 
 
 if __name__ == '__main__':
